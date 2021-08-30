2f11e9413a14442cc80498cfc0505679604aa9a3
MMPgenave <77011306+MMPgenave@users.noreply.github.com>
2021-08-30 13:23:34 +0430

    constructing the grid (CSS)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import "./style.css";
+
 function App() {
   return (
     <>
-      <h1>Slider</h1>
+      <div className="header">
+        <h1 className="backslash">/</h1>
+        <h1 className="title">Reviews</h1>
+      </div>
+      <div className="container">
+        <div className="LeftArrow">LA</div>
+        <div className="RightArrow">RA</div>
+        <div className="content">content</div>
+        <div className="quote">qutation</div>
+      </div>
     </>
   );
 }
