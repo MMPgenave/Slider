722faee3b2ccf84aa7ae3f68f0a25712d6a530f3
MMPgenave <mohammadmp14@gmail.com>
2021-09-03 20:59:37 +0430

    initial setup

--- src/App.js
+++ src/App.js
@@ -7,54 +7,12 @@ import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 
 function App() {
-  const [person, setPerson] = useState(Data[0]);
-  const [flag, setFlag] = useState(true);
-
-  //auto-slider function
-
-  console.log(`flag=${flag}`);
-
-  if (flag) {
-    setTimeout(() => {
-      setPerson((I) => {
-        if (person.id === 5) {
-          return Data[0];
-        } else {
-          return Data[person.id];
-        }
-      });
-    }, 5000);
-  }
-
-  if (!flag) {
-    setInterval(() => {
-      setFlag(true);
-    }, 5000);
-  }
-  // setInterval(()=>console.log("Hello!"),2000)
-  //useEffect(()=>{setInterval(()=>console.log("Hello!"),2000)},[]);
-
-  const prevPicture = (ID) => {
-    setFlag(false);
-    setPerson((I) => {
-      if (ID === 1) {
-        return Data[4];
-      } else {
-        return Data[ID - 2];
-      }
-    });
-  };
+  const [people, setPeople] = useState(Data);
+  const [index, setTindex] = useState(0);
+  const prevPicture = (ID) => {};
 
   const nextPicture = () => {
     console.log("next button clicked");
-    setFlag(false);
-    setPerson((I) => {
-      if (person.id === 5) {
-        return Data[0];
-      } else {
-        return Data[person.id];
-      }
-    });
   };
 
   //console.log(sec);
@@ -62,30 +20,27 @@ function App() {
   return (
     <>
       <div className="header">
-        <h1 className="backslash">/</h1>
-        <h1 className="title">Reviews</h1>
+        <h1 className="title">
+          {" "}
+          <span>/</span> Reviews
+        </h1>
       </div>
-      <div className="container">
-        <div className="LeftArrow">
-          <button
-            type="button"
-            className="LA"
-            onClick={() => prevPicture(person.id)}
-          >
-            <FiChevronLeft />
-          </button>
-        </div>
-        <div className="RightArrow">
-          <button type="button" className="RA" onClick={() => nextPicture()}>
-            <FiChevronRight />
-          </button>
-        </div>
-        <div className="content">
-          <Person info={person} />
-        </div>
-        <div className="quote">
-          <FaQuoteRight />
-        </div>
+      <div className="section_container">
+        {people.map((person, indexx) => {
+          const { id, name, job, image, text } = person;
+          let position = "nextSlide";
+          if (indexx === index) {
+            position = "activeSlide";
+          }
+          if ( indexx === 4) {
+            position = "lastSlide";
+          }
+          return (
+            <article key={id} className={position}>
+              <img src={image} className="image" alt={name} />
+            </article>
+          );
+        })}
       </div>
     </>
   );
