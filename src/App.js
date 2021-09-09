a003d2caab08c543e7c30413b06180675420c5e7
MMPgenave <mohammadmp14@gmail.com>
2021-09-09 21:49:22 +0430

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
 import React from "react";
 import "./style.css";
 import Data from "./data";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 import Person from "./Person";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 
 function App() {
   const [person, setPerson] = useState(Data[0]);
-  const [flag, setFlag] = useState(true);
+  const refFlag = useRef(true);
 
   //auto-slider function
+  console.log(`flag=${refFlag.current}`);
 
-  console.log(`flag=${flag}`);
-
-  if (flag) {
+  if (refFlag.current) {
     setTimeout(() => {
-      setPerson((I) => {
-        if (person.id === 5) {
+      setPerson((prevValue) => {
+        if (person.id === 4) {
           return Data[0];
         } else {
-          return Data[person.id];
+          return Data[person.id+1];
         }
       });
     }, 5000);
   }
+ 
+  
+  useEffect(() => {
+    console.log('useEffect enabeld')
+    refFlag.current = true;
+    
+  })
 
-  if (!flag) {
-    setInterval(() => {
-      setFlag(true);
-    }, 5000);
-  }
-  // setInterval(()=>console.log("Hello!"),2000)
-  //useEffect(()=>{setInterval(()=>console.log("Hello!"),2000)},[]);
+  const prevPicture = () => {
+        console.log("Previous button clicked");
 
-  const prevPicture = (ID) => {
-    setFlag(false);
-    setPerson((I) => {
-      if (ID === 1) {
+    refFlag.current = false;
+    setPerson((prevValue) => {
+      if (person.id === 0) {
         return Data[4];
       } else {
-        return Data[ID - 2];
+        return Data[person.id - 1];
       }
     });
   };
 
   const nextPicture = () => {
     console.log("next button clicked");
-    setFlag(false);
-    setPerson((I) => {
-      if (person.id === 5) {
+    refFlag.current = false;
+    setPerson((prevValue) => {
+      if (person.id === 4) {
         return Data[0];
       } else {
-        return Data[person.id];
+        return Data[person.id + 1];
       }
     });
   };
 
-  //console.log(sec);
+
 
   return (
     <>
@@ -70,13 +70,17 @@ function App() {
           <button
             type="button"
             className="LA"
-            onClick={() => prevPicture(person.id)}
+            onClick={() => prevPicture()}
           >
             <FiChevronLeft />
           </button>
         </div>
         <div className="RightArrow">
-          <button type="button" className="RA" onClick={() => nextPicture()}>
+          <button
+            type="button"
+            className="RA"
+            onClick={() => nextPicture()}
+          >
             <FiChevronRight />
           </button>
         </div>
--- src/Person.js
+++ src/Person.js
@@ -7,6 +7,8 @@ const Person = (props) => {
     <div key={id} className="person">
       <img src={image} alt={name} className="image" />
       <h1>{name}</h1>
+      <h3>{job}</h3>
+      <p >{text}</p>
     </div>
   );
 };
